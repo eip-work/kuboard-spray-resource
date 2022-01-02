77b46002f0adf74cba96eb0222c1fe2f22fddcb1
Shao Huan Qing <shaohq@foxmail.com>
2022-01-03 06:29:07 +0800

    优化多线程下载镜像

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM alpine:3.15.0
 
-ADD ./3rd /kuboard-spray/resource/content/3rd
-ADD ./roles /kuboard-spray/resource/content/roles
-ADD ./ansible.cfg ./README.md ./package.yaml /kuboard-spray/resource/content/
-ADD ./kubespray_cache/ /kuboard-spray/resource/content/kubespray_cache/
+COPY 3rd/ /kuboard-spray/resource/content/3rd/
+COPY roles/ /kuboard-spray/resource/content/roles/
+COPY kubespray_cache/  /kuboard-spray/resource/content/kubespray_cache/
+COPY image_cache_k8s/ /kuboard-spray/resource/content/kubespray_cache/images/
+COPY image_cache/ /kuboard-spray/resource/content/kubespray_cache/images/
+COPY ansible.cfg README.md *.yaml *.md /kuboard-spray/resource/content/
--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # tag=swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard-spray-resource
-tag=registry.cn-shanghai.aliyuncs.com/kuboard-spray/kuboard-spray-resource
-tag_backup=eipwork/kuboard-spray-resource
+tag=$(cat package.yaml | shyaml get-value metadata.available_at.0)
 
 version=$(cat package.yaml | shyaml get-value metadata.version)
 kuboardspray_version="${version#*spray-}"
@@ -24,20 +23,41 @@ cd ../..
 
 echo
 echo "链接到 /cache/${version}"
-mkdir "./cache/${version}" || true
-ln -s "./cache/${version}" kubespray_cache
-ls -lh kubespray_cache
+
+# 挪一下文件位置，以便将目录映射到镜像不同的 layer
+rm kubespray_cache
+mkdir image_cache_k8s
+mv ./cache/${version}/images/k8s.* image_cache_k8s/
+mv ./cache/${version}/images image_cache
+mv ./cache/${version} kubespray_cache
+
 
 echo
 echo "构建镜像"
 
 docker build -f Dockerfile -t $tag:$version .
 
+# 将文件恢复到原来的位置
+mv kubespray_cache ./cache/${version}
+mv image_cache ./cache/${version}/images
+mv image_cache_k8s/k8s.* ./cache/${version}/images/
+rm -r image_cache_k8s
+
+ln -s "./cache/${version}" kubespray_cache
+ls -lh kubespray_cache
 
 echo
 echo "推送镜像"
 docker push $tag:$version
 
-docker tag $tag:$version $tag_backup:$version
+available_at_length=$(cat package.yaml | shyaml get-length metadata.available_at)
 
-docker push $tag_backup:$version
+echo $available_at_length
+for ((i = 1; i < ${available_at_length}; i++)) do
+  tag_backup=$(cat package.yaml | shyaml get-value metadata.available_at.${i})
+  echo ""
+  echo "推送镜像 ${tag_backup}"
+  docker tag $tag:$version $tag_backup:$version
+  docker push $tag_backup:$version
+done
+
