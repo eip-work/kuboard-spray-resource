a78bf444e17b99b3ad5aa95e8564a0ccc166690e
Shao Huan Qing <shaohq@foxmail.com>
2022-01-27 19:59:34 +0800

    支持卸载可选组件

---
+++ action_plugins/kube.py
@@ -0,0 +1,21 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        print(task_vars['kuboardspray_operation'])
+        module_args = self._task.args.copy()
+        if task_vars['kuboardspray_operation'] == 'remove_addon':
+            if (module_args['resource'] == 'ns' and module_args['namespace'] == 'default'):
+                print("ignore default namespace.")
+            else:
+                module_args['state'] = 'absent'
+
+        module_return = self._execute_module(module_name='kube',
+                                             module_args=module_args,
+                                             task_vars=task_vars, tmp=tmp)
+        return module_return
